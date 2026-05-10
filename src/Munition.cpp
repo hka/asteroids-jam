04c474fcb9ad9e26f14e7bbd25bdb58fbdde4255
SkitzFist <jo.persson1@hotmail.se>
2026-05-10 20:54:25 +0200

    Adapt to new raylib version
    * Raylib now comes with vector overloading
    
    * raylib.DrawCricleGradient now takes vector2 instead of floats as pos

--- src/Munition.cpp
+++ src/Munition.cpp
@@ -51,7 +51,7 @@ void DrawShoots(const std::vector<Shoot>& shoots)
   for(const Shoot& s : shoots)
   {
     //DrawCircleV(s.data.position, s.data.radius, DARKGRAY);
-    DrawCircleGradient(s.data.position.x, s.data.position.y, s.data.radius, WHITE, BLUE);
+    DrawCircleGradient(s.data.position, s.data.radius, WHITE, BLUE);
   }
 }
 
